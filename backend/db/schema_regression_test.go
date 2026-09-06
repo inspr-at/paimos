169769f15f36a1f90b5704dcc5512451c09e039e
Markus Barta <markus@barta.com>
2026-09-06 15:01:47 +0200

    test(db): expect latest schema migration 178
    Signed-off-by: Markus Barta <markus@barta.com>
    (cherry picked from commit b351d2bfa385f007f093d5f96e1bfa7d7833cbee)
    Signed-off-by: Markus Barta <markus@barta.com>

--- backend/db/schema_regression_test.go
+++ backend/db/schema_regression_test.go
@@ -32,7 +32,7 @@ func schemaNames(t *testing.T, database *sql.DB, query string) []string {
 	return names
 }
 
-const latestSchemaVersion = 177
+const latestSchemaVersion = 178
 
 func TestMigration177PreservesAttentionLedgerAndSequence(t *testing.T) {
 	database, err := sql.Open("sqlite", filepath.Join(t.TempDir(), "m177.db")+"?_txlock=immediate")
